82aa50df61abe9222869feca8f4354901638ca86
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-09 13:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your code here
+const images = document.querySelectorAll("#parent>img")
+const container = document.getElementById("parent")
+
+for(let i=0; i<images.length; i++)
+{
+images[i].addEventListener("dragover",ondragover)
+images[i].addEventListener("dragstart",ondragstart)
+images[i].addEventListener("drop",ondrop)
+}
+
+
+function ondrop(event) {
+	 event.preventDefault();
+	
+let sourceId = event.dataTransfer.getData("sourceId")	
+let srcElement = document.getElementById(sourceId)
+let desElement = event.target;
+
+
+let NsrcElement =  srcElement.nextElementSibling;
+let NdesElement = desElement.nextElementSibling;
+
+
+container.insertBefore(desElement,NsrcElement)
+container.insertBefore(srcElement,NdesElement)
+
+	
+}
+
+
+function ondragover(event){
+event.preventDefault();
+}
+
+function ondragstart(event){
+event.dataTransfer.setData("sourceId",event.target.id)
+}
+
+
+
